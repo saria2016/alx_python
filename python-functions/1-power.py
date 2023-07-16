353daa9f538eb08531e9c13d8a2fc574d84cce0b
saria2016 <saria1904@gmail.com>
2023-07-16 07:01:00 -0700

    power

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,2 +1,16 @@
 def pow(a, b):
-    return a ^ b
+    # Initialize result to 
+    result = 1
+    # If b is negative, invert a and make b positive
+    if b < 0:
+        a = 1/a
+        b = -b
+    # Compute a to the power of b using repeated multiplication
+    while b > 0:
+        # If b is odd, multiply result by a
+        if b % 2 == 1:
+            result *= a
+            # Square a and halve b
+        a *= a
+        b //= 2
+    return result
