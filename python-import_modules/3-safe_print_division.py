7604cd96956c4faddebe05934f6a1e072157a945
saria2016 <saria.m.mohammed@gmail.com>
2023-07-25 12:12:15 +0300

    fix try and except

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-def safe_print_division(my_list=[]):
-    new = []
-    for num in my_list:
-        if num % 2 == 0:
-            new.append(True)
-        else:
-            new.append(False)
-    return new
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        return None
+    else:
+        return result
+    finally:
+        print("Inside result: {}".format(result))
