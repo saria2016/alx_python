6c19195cc2c231898bca1ec573b43c62c55643f2
saria-mohi <saria.m.mohammed@gmail.com>
2023-07-29 15:48:50 +0300

    fix error

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -4,7 +4,7 @@ Creates a Rectangle class.
 """
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
