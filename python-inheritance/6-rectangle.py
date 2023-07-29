9fc1fb65bd9bc45f432a0e09078dce7ae17fa00e
saria2016 <saria1904@gmail.com>
2023-07-29 05:14:37 -0700

    fix code style

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -6,7 +6,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-   
+
     def __init__(self, width, height):
 
         self.integer_validator("width", width)
