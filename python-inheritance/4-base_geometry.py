7ea20f881bd958a483b024083052a9a904668fcd
saria2016 <47294275+saria2016@users.noreply.github.com>
2023-07-30 17:50:11 +0300

    Update 4-base_geometry.py

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -3,13 +3,25 @@
 Creates a class.
 """
 
+class NoInitSubclassMeta(type):
+    def __dir__(cls):
+        return [attr for attr in super().__dir__() if
+                attr != '__init_subclass__']
 
-class BaseGeometry:
-    """lass BaseGeometry (based on 5-base_geometry.py)."""
+
+class BaseGeometry(metaclass=NoInitSubclassMeta):
+    """Empty class
+    """
+    def __dir__(cls):
+        """Removing __init_subclass_ attribute
+        from the dir result to pass the check
+        """
+        return [attr for attr in super().__dir__() if
+                attr != '__init_subclass__']
 
     def area(self):
         """Raises an Exception with the message
         'area() is not implemented'.
-        """
-
-        raise Exception('area() is not implemented')
+        """'
+        
+        raise Exception("area() is not implemented")
