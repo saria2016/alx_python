edb4f0c9d0fcf3ba48b21b62614f56984876fb48
saria-mohi <saria.m.mohammed@gmail.com>
2023-08-07 23:09:58 +0300

    task0 create base

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a class Base"""
+
+
+class Base:
+    """Class that defines properties of Base.
+
+    Attributes:
+        id (int): Identity of each instance.
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Creates new instances of Base.
+
+        Args:
+            id (int, optional): Identity of each instance. Defaults to None.
+        """
+        if id == None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
+        
