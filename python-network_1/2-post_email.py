d691bb114297013c1d221a4f8619bcb1fad135d2
saria-mohi <saria.m.mohammed@gmail.com>
2023-08-10 17:26:01 +0300

    fix error

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """A script that:
-- post Email
+- takes in a URL
+- sends a POST request to the passed URL
+- takes email as a parameter
+- displays the body of the response
 """
-import sys
 import urllib.parse
-import urllib.request
+import requests
+import sys
 
 
 if __name__ == "__main__":
@@ -12,6 +15,6 @@ if __name__ == "__main__":
     value = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(value).encode("ascii")
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
+    request = requests(url, data)
+    with requests.urlopen(request) as response:
         print(response.read().decode("utf-8"))
