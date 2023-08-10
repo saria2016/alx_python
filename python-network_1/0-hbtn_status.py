3caafb6edb2a8c4753cb73a5fc9d89231225bb5f
saria-mohi <saria.m.mohammed@gmail.com>
2023-08-10 17:17:33 +0300

    import modul

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """A script for fetches https://alx-intranet.hbtn.io/status status
 """
-
+import requests
 
 if __name__ == '__main__':
-    import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    url = 'https://alx-intranet.hbtn.io/status'
+    r = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
