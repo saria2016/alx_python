1c7d0eb48ff398470e5972a4c4798498c31e319e
saria-mohi <saria.m.mohammed@gmail.com>
2023-08-10 17:27:17 +0300

    fix other import

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -5,16 +5,14 @@
 - takes email as a parameter
 - displays the body of the response
 """
-import urllib.parse
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
-
-    request = requests(url, data)
-    with requests.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    email = sys.argv[2]
+    payload = {
+        "email": email
+    }
+    response = requests.post(url, data=payload)
+    print(response.text)
