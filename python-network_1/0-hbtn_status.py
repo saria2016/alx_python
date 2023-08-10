f638d5fe8970748b6c8b660b71b8b637c8ec29cd
saria-mohi <saria.m.mohammed@gmail.com>
2023-08-10 17:18:26 +0300

    fix error

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ import requests
 if __name__ == '__main__':
 
     url = 'https://alx-intranet.hbtn.io/status'
-    r = requests.get(url)
+    result = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(result.text)))
+    print("\t- content: {}".format(result.text))
