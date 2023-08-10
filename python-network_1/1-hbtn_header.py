ca4adac31375970c486d1b18512480ae8e9a62c7
saria-mohi <saria.m.mohammed@gmail.com>
2023-08-10 17:21:28 +0300

    import fix error name

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,7 +2,7 @@
 """A script that:
 - sends a request to the URL and displays the value of the variable
 """
-import request, sys
+import requests, sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
