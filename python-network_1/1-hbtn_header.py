8f2810f1199003553cc82a4dfd2b057802838980
saria-mohi <saria.m.mohammed@gmail.com>
2023-08-10 17:22:10 +0300

    import sys

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,7 +2,8 @@
 """A script that:
 - sends a request to the URL and displays the value of the variable
 """
-import requests, sys
+import requests
+import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
