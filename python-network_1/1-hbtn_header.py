b0c5b5c0fac31b5351443a080cb355c03eafe632
saria-mohi <saria.m.mohammed@gmail.com>
2023-08-10 16:18:38 +0300

    sends a request to the URL

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A script that:
+- sends a request to the URL and displays the value of the variable
+"""
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
