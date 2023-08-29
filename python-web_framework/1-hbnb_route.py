d730e0434e4c62e982ea3da24ad8001f2743a618
saria2016 <saria1904@gmail.com>
2023-08-29 11:49:24 +0300

    hbnb route

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" task on flask"""
+from flask import Flask
+app = Flask(__name__)
+
+
+# this rout by default called when run it
+@app.route("/", strict_slashes=False)
+def display():
+    # this is the method to dispaly message for user
+    return("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def show_message():
+    # Dispaly HBNB message for user
+    return("HBNB")
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000)
