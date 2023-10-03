14530d83140e0926e9ee3426f7c913e55a61c414
saria-mohi <saria.m.mohammed@gmail.com>
2023-10-03 19:19:49 +0300

    Export to CSV file fix error

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,29 +1,36 @@
+#!/usr/bin/python3
+"""
+Python script to export data in the CSV format.
+"""
+
 import csv
 import requests
 import sys
 
 
-def export_to_csv(employee_id):
-    # Get employee details
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-    employee_name = user_data["username"]
+def export_to_CSV(user_id):
+    employee_name = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    ).json()["name"]
+    tasks = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
+    ).json()
 
-    # Get TODO list for the employee
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-    todos_response = requests.get(todos_url)
-    todos_data = todos_response.json()
+    tasks_data = []
 
-    # Create and write to CSV file
-    filename = f"{employee_id}.csv"
-    with open(filename, mode="w", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for task in todos_data:
-            writer.writerow([employee_id, employee_name, task["completed"], task["title"]])
+    for task in tasks:
+        tasks_data.append(
+            [
+                str(user_id),
+                employee_name,
+                task["completed"],
+                task["title"],
+            ]
+        )
 
-    print(f"Data exported to {filename} successfully.")
+    with open(str(user_id) + ".csv", "w", encoding="UTF8", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(tasks_data)
 
 
 if __name__ == "__main__":
@@ -33,6 +40,6 @@ if __name__ == "__main__":
 
     try:
         employee_id = int(sys.argv[1])
-        export_to_csv(employee_id)
+        export_to_CSV(employee_id)
     except ValueError:
         print("Please provide a valid employee ID.")
